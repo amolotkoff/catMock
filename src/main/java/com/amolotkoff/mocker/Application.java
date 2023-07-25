009ab21731d441724402377a115756ec052b2a5f
Kozloder <andrew.molotkoff@gmail.com>
2023-07-26 02:24:34 +0700

    app

--- src/main/java/com/amolotkoff/mocker/Application.java
+++ src/main/java/com/amolotkoff/mocker/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        try {
+            SpringApplication.run(Application.class, args);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
